8ee056335a2c746b784e161dd7aa05343f7283a3
Luca Iovio <luca.iovio@omnys.com>
2017-03-23 17:47:48 +0100

    updated release script

--- docker-image-push.sh
+++ docker-image-push.sh
@@ -5,15 +5,24 @@ set -ex
 IMAGE_NAME=omnys/alpine-emqtt
 VERSION=2.0.7
 
-#build image
-docker build -t ${IMAGE_NAME} .
 
-#tag image with latest version
-docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${VERSION}
+read -p "Did you update VERSION value and commited code? Y to continue, CTRL-C to abort." -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  #build image
+  docker build -t ${IMAGE_NAME} .
 
-#login to docker hub
-# docker login
-#
-# #push latest and tagged images
-# docker push ${IMAGE_NAME}:latest
-# docker push ${IMAGE_NAME}:${VERSION}
+  #tag image with latest version
+  docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${VERSION}
+
+  #push tags
+  git push --tags
+
+  #login to docker hub
+  # docker login
+  #
+  # #push latest and tagged images
+  # docker push ${IMAGE_NAME}:latest
+  # docker push ${IMAGE_NAME}:${VERSION}
+fi
